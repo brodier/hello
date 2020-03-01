030fc6b8611b10ed2e55492aef240f56ab675a61
Bernard Rodier <bernard.rodier@gmail.com>
2020-03-01 16:20:37 +0100

    Formatting package

--- hello.go
+++ hello.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"fmt"
-  "strings"
 	"github.com/brodier/hello/morestrings"
-  "github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp"
+	"strings"
 )
 
 func main() {
 	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
-  fmt.Println(cmp.Diff("Hello World", "Hello Go"))
-  fmt.Println(strings.Map(morestrings.Rot13, "'Twas brillig and the slithy gopher..."))
+	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
+	fmt.Println(strings.Map(morestrings.Rot13, "'Twas brillig and the slithy gopher..."))
 }
