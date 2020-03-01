3356f740711055d4acb47e475a3798a124b57771
Bernard Rodier <bernard.rodier@gmail.com>
2020-03-01 15:36:09 +0100

    Using package morestrings

--- hello.go
+++ hello.go
@@ -1,7 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/brodier/hello/morestrings"
+)
 
 func main() {
-	fmt.Printf("hello, world\n")
+	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
 }
